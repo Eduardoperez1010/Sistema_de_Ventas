42078112e3c44e5524fc60ae0ef6652393fadd20
Eduardo Perez <lalo383596@gmail.com>
2020-08-20 21:26:58 -0500

    Agregando un controlador nuevo

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use sistemaVentas\Http\Request;
+use sistemaVentas\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use sistemaVentas\Http\Request\CategoriaFormRequest;
+use DB;
+
+
+class CategoriaController extends Controller
+{
+    
+	public function _construct(){
+		
+	}
+
+	public function index(){
+		
+	}
+
+	public function create(){
+
+	}
+
+	public function store(){
+
+	}
+
+	public function show(){
+
+	}
+
+	public function edit(){
+
+	}
+
+	public function update(){
+
+	}
+
+	public function destroy(){
+
+	}
+
+}
